0788bc7f83ae4df4b53eaf492265ee41f2d13895
zachtosi <zachtosi@gmail.com>
2016-10-03 16:20:40 -0400

    Where to start? In these early stages I'm adding and removing so much each time... these messages will get more descriptive when the code-base stablizes... which should happen in short order at this point. A larget amount of cleanup has been done to unify nomenclatures across different classes (everyone calls their copy of neurons 'neuHost' now, for instance). IP and HP have been reorganized to new specifications, and are almost ready. SynNormalization still needs to be redone as does the ordering of events in SynMatrices propagate_selective. Everything is in place to position neurons in 3D space. Most importantly SOModule has been renamed Module and now MANA_Module is the module to be used for the MANA network. SORN_Module has been added but not implemented. MANA_Module is completely finished pending changes to the plasticity/neuron components and possible requests from other classes. This is significant as MANA_Module provides the overall layout of the structure of code and order of updates appropriate for the model (i.e. how all the mechanisms fit together when the model is actually being run).

---
+++ network/src/Module.hpp
@@ -0,0 +1,120 @@
+#include <arrayfire.h>
+#include <cstdint>
+#include "AIFNeuron.h"
+#include "SynMatrices.h"
+#include "HPComponent.h"
+#include "IPComponent.h"
+#include "SynActors.h"
+#include "UDFPlasticity.h"
+#include "Network.h"
+
+#ifndef SOMODULE_H_
+#define SOMODULE_H_
+
+#define DEF_INH_TRIGGER 1.4f
+#define DEF_EXC_TRIGGER 0.9f
+#define DEF_IE_RATIO 0.2f
+
+using namespace af;
+
+////////////////////////////////////////////////////////////////////////////
+// Self-Organizing Module Base Class
+// An SOModule contains a source set of neurons, a target set of neurons,
+// and a set of synapses connecting them. It also contains all the required
+// classes to carry out the full set of self-organizing functions aside
+// from synaptic pruning or growth. A module can be created from 
+// 1 or 2 existing sets of neurons, attached to an extant Neu->syn->neu
+// tuple or used to create an entire complete neu->syn->neu tuple with
+// all the necessary plasticity components.
+////////////////////////////////////////////////////////////////////////////
+
+class Module
+{
+
+	public:
+
+		Network &host;
+		const uint32_t size;
+		const uint32_t numExc;
+		const uint32_t numInh;
+
+		FREstimator getExcWatcher()
+		{
+			return *watcherExc;
+		}
+		FREstimator getExcWatcher()
+		{
+			return *watcherInh;
+		}
+
+		Module(	const Network &_host,
+						const uint32_t _size,
+						const Position _minPos,
+						const Position _maxPos	);
+
+		Module (	const Network &_host,
+						const uint32_t _size,
+						const Position _minPos,
+						const Position _maxPos, 
+						const float _ieRatio	);
+
+	protected:
+		
+		AIFNeuron &excNeuGrp;
+		AIFNeuron &inhNeuGrp;
+		std::vector<SynMatrices*> synGrps; 
+		FREstimator* watcherExc;
+		FREstimator* watcherInh;
+
+	friend class DataRecorder;	
+};
+
+
+class SORN_Module 
+{
+	// TODO
+
+	friend class DataRecorder;
+};
+
+class MANA_Module
+{
+		// TODO: More constructors, specify STDP, etc.
+
+		MANA_Module(	const Network &_host,
+						const uint32_t _size,
+						const Position _minPos,
+						const Position _maxPos	);
+
+		MANA_Module (	const Network &_host,
+						const uint32_t _size,
+						const Position _minPos,
+						const Position _maxPos, 
+						const float _ieRatio	);
+		~MANA_Module();
+
+		void iterateOne();
+		void runForward(uint32_t numIters);
+
+	private:
+
+		HPComponent* hpExc;
+		HPComponent* hpInh;
+
+		IPComponent* ipExc;
+		IPComponent* ipInh;
+
+		SynNormalizer* sNrmExc;
+		SynNormalizer* sNrmInh;
+
+		UDFPlasticity* eeUDF;
+		UDFPlasticity* eiUDF;
+		UDFPlasticity* ieUDF;
+		UDFPlasticity* iiUDF;
+
+
+	friend class DataRecorder;
+
+};
+
+#endif // SOMODULE_H_
